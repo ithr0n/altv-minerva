172a4ecc33cbe37e023b038dfd277306eff321e7
ithr0n <ithr0n@live.de>
2020-12-06 22:06:20 +0100

    enable force first person

--- src/PlayGermany.Client/Handlers/PedHandler.ts
+++ src/PlayGermany.Client/Handlers/PedHandler.ts
@@ -6,10 +6,10 @@ natives.setFollowPedCamViewMode(4)
 natives.setFollowVehicleCamViewMode(4)
 
 alt.everyTick(() => {
-    //natives.disableControlAction(0, 0, true)
+    natives.disableControlAction(0, 0, true)
 
     if (natives.getVehiclePedIsEntering(alt.Player.local.scriptID) !== 0) {
-        //natives.setFollowPedCamViewMode(4);
+        natives.setFollowPedCamViewMode(4);
     }
 })
 
